b878b977dc8260cdad82d7ab44cc51575624e34c
Marc Bouvier <m.bouvier.dev@gmail.com>
2021-11-27 14:41:56 +0100

    refactor to idiomatic Kotlin

--- shared-domain/src/commonMain/kotlin/fr/baldir/fizzbuzz/domain/FizzBuzz.kt
+++ shared-domain/src/commonMain/kotlin/fr/baldir/fizzbuzz/domain/FizzBuzz.kt
@@ -1,8 +1,8 @@
 package fr.baldir.fizzbuzz.domain
 
 fun fizzbuzz(i: Int): String = when {
-    i % 15 ==0 -> "FizzBuzz"
-    i % 3 ==0 -> "Fizz"
-    i % 5 ==0 -> "Buzz"
-    else -> i.toString()
+    i % 15 == 0 -> "FizzBuzz"
+    i % 3 == 0 -> "Fizz"
+    i % 5 == 0 -> "Buzz"
+    else -> "$i"
 }
